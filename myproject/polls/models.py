c5064c922b2b2864decf36771c60d5dcac0c64b4
MariaPiaGelos <gelos.mariapia@gmail.com>
2020-08-27 14:33:23 -0700

    templates

--- myproject/polls/models.py
+++ myproject/polls/models.py
@@ -11,6 +11,9 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -18,7 +21,6 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
-
 
 
 
